6ed82eab2f9ffd9826fa6951f4ef34fdbdae5d6c
koharashota <transparent.blue69@gmail.com>
2016-09-10 14:26:15 +0900

    ニューラルネットワーク(フォワードプロパゲーション)を使った数値予想が完了

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,11 +21,10 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+z2_whitout_0 = sigmoid([ones(size(X, 1), 1), X] * Theta1'); % z2の値が0番目の要素なしでかえる
+z3 = sigmoid([ones(size(z2_whitout_0, 1), 1), z2_whitout_0] * Theta2');
 
-
-
-
-
+[dummy, p] = max(z3, [], 2);
 
 
 
